cbb12d7b5848945a3825f070425505da46b6b942
Ernesto <jorge.cywdt@gmail.com>
2023-04-17 11:24:16 -0500

    Cambios realizados por SISTEMASUNI
    ---- Gitignore ----
    
    - Gitignore for a Django project
    https://djangowaves.com/tips-tricks/gitignore-for-a-django-project/
    
    - Permitimos SQLite3 en Gitignore

--- Contactos/urls.py
+++ Contactos/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for Contactos project.
+"""Contactos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Aplicaciones.Gestion.views import formularioContacto, contactar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('formularioContacto/', formularioContacto),
+    path('contactar/', contactar)
 ]
